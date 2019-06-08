270b15dcab3955b1a8ce60586e8e7b8d9e277e9c
BlueScreen <l.chougrad@gmail.com>
2019-06-08 13:21:24 +0200

    wip on login and registration

--- client/drinkly/src/app/app-routing.module.ts
+++ client/drinkly/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home',canActivate: [AuthGuard], loadChildren: './home/home.module#HomePageModule'}
 ];
 
 @NgModule({
--- client/drinkly/src/app/home/home.page.ts
+++ client/drinkly/src/app/home/home.page.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { NativeGeocoderOptions, NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+import { ModalController, PopoverController, ToastController, LoadingController } from '@ionic/angular';
+import { ProposalService } from '../_services/proposal.service';
+import { UserService } from '../_services/user.service';
+import { AuthService } from '../_services/auth.service';
+import { Place } from '../autocomplete-input.component';
+
+
+const USE_OWN_LOCATION = 'useMyPosition'
+const USE_OWN_LANGUAGE = 'useMyLanguage'
 
 @Component({
   selector: 'app-home',
@@ -6,9 +16,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  flipped = false
-  constructor() {}
+  filterSelectOptions = {
+    header: 'filter',
+    // subHeader: 'pllllll',
+    // message: '$1.00 per topping',
+  };
 
+  filter = [USE_OWN_LOCATION, USE_OWN_LANGUAGE]
+  flipped = false
+
+  lastSelectedZone : Place
+  geoLatitude: number;
+  geoLongitude: number;
+  geoAccuracy:number;
+  geoAddress: string;
+  useMyPosition = true
+  useMyLanguage = true
+  maxDistance = 5000
+  watchLocationUpdates:any; 
+  loading:any;
+  isWatching:boolean;
+  userImg:string
+  //Geocoder configuration
+  geoencoderOptions: NativeGeocoderOptions = {
+    useLocale: true,
+    maxResults: 5
+  };
+  constructor(
+    private modalController: ModalController,
+    private proposalService: ProposalService,
+    private userService: UserService,
+    private geolocation: Geolocation,
+    public popoverController: PopoverController,
+    private toastCtrl: ToastController,
+    private authService: AuthService,
+    private loadingCtrl : LoadingController,
+    private nativeGeocoder: NativeGeocoder
+  ) {
+    console.log('this.authService.currentUserValue')
+    this.userImg = this.authService.currentUserValue.profileImg
+    console.log(this.authService.currentUserValue)
+  }
+ 
   flipIt() {
     this.flipped = !this.flipped;
   }
