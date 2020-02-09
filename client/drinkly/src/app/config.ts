9dda5973761f4e0bae173938b3f8305331bb019a
luigichougrad <gigibit92@gmail.com>
2020-02-09 19:26:14 +0100

    fixes project structure

--- client/drinkly/src/app/config.ts
+++
@@ -1,3 +0,0 @@
-export const AUTH_SERVER                = 'http://10.30.31.205:8080'
-export const SERVICE_SERVER             = 'http://10.30.31.205:8080'
-export const WEBSOCKET_SERVICE_SERVER   = 'http://10.30.31.205:8080/chat-websocket'
--- client/kofify/kofify/src/app/config.ts
+++
@@ -1,3 +0,0 @@
-export const AUTH_SERVER                = 'https://kofify.com'
-export const SERVICE_SERVER             = 'https://kofify.com'
-export const WEBSOCKET_SERVICE_SERVER   = 'https://kofify.com/chat-websocket'
