d6ab515d78aa8f2e493f8e8fb71d815249f90f2c
alexey <khlebtsov@gmail.com>
2020-02-18 09:15:05 +0100

    strange commit

--- client/kofify/www/polyfills-es2015.js
+++ client/kofify/www/polyfills-es2015.js
@@ -6393,8 +6393,8 @@ window.__Zone_disable_customElements = true;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/luigichougrad/archimedia/projects/kokify/icebreaker/client/kofify/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Users/luigichougrad/archimedia/projects/kokify/icebreaker/client/kofify/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/alexey/AppProjects/icebreaker/client/kofify/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/alexey/AppProjects/icebreaker/client/kofify/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
